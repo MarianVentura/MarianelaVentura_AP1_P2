00e954e1e516a30c8735207d4c00d8e9fb34a394
MarianVentura <marianv1119@gmail.com>
2024-11-18 19:10:42 -0400

    Cree el modelo de combosDetalle

---
+++ Models/CombosDetalle.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarianelaVentura_AP1_P2.Models;
+
+public class CombosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [Required(ErrorMessage = "El ComboId es obligatorio.")]
+    public int ComboId { get; set; }
+
+    [ForeignKey("ComboId")]
+    public Combos? Combo { get; set; }
+
+    [Required(ErrorMessage = "El ArticuloId es obligatorio.")]
+    public int ArticuloId { get; set; }
+
+    [ForeignKey("ArticuloId")]
+    public Articulos? Articulo { get; set; }
+
+    [Required(ErrorMessage = "La cantidad es obligatoria.")]
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser al menos 1.")]
+    public int Cantidad { get; set; }
+
+    [Required(ErrorMessage = "El costo es obligatorio.")]
+    [Range(0, double.MaxValue, ErrorMessage = "El costo debe ser un valor positivo.")]
+    public decimal Costo { get; set; }
+}
