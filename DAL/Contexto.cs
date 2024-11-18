f41d68d51b2c4ddd19fb98111c80d1004770e62b
MarianVentura <marianv1119@gmail.com>
2024-11-18 19:09:39 -0400

    Agregue Combos y CombosDetalle al contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,6 +8,8 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     public DbSet<Articulos> Articulos { get; set; }
+    public DbSet<Combos> Combos {  get; set; }
+    public DbSet<CombosDetalle> CombosDetalle {  get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
