4a82b5b1b48ce285bd0ad467c908b62c1cb31ca5
MarianVentura <marianv1119@gmail.com>
2024-11-18 10:14:08 -0400

    Contexto trabajado

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MarianelaVentura_AP1_P2.Models;
+
+namespace MarianelaVentura_AP1_P2.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Registro> Registros { get; set; }
+}
