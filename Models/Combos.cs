7c95a2ced7d4ac977d230cfb9bd3c98a5db26e53
MarianVentura <marianv1119@gmail.com>
2024-11-18 19:10:30 -0400

    Cree el modelo de Combos

---
+++ Models/Combos.cs
@@ -0,0 +1,27 @@
+﻿using MarianelaVentura_AP1_P2.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarianelaVentura_AP1_P2.Models;
+
+public class Combos
+{
+    [Key]
+    public int ComboId { get; set; }
+
+    [Required(ErrorMessage = "La fecha es obligatoria.")]
+    public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "La descripción es obligatoria.")]
+    [StringLength(100, ErrorMessage = "La descripción no puede exceder los 100 caracteres.")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "El precio es obligatorio.")]
+    [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser un valor positivo.")]
+    public decimal Precio { get; set; }
+
+    [Required(ErrorMessage = "El estado de venta es obligatorio.")]
+    public bool Vendido { get; set; }
+
+    public ICollection<CombosDetalle>? CombosDetalles { get; set; } = new List<CombosDetalle>();
+}
