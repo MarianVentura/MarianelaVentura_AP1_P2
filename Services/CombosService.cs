cd6a8383f8e3897923c031e0ee5543a5de783bed
MarianVentura <marianv1119@gmail.com>
2024-11-25 11:42:01 -0400

    Modifique el Services de Combos

--- Services/CombosService.cs
+++ Services/CombosService.cs
@@ -1,70 +1,67 @@
 ﻿using MarianelaVentura_AP1_P2.DAL;
 using MarianelaVentura_AP1_P2.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Linq.Expressions;
 
-namespace MarianelaVentura_AP1_P2.Services
+namespace MarianelaVentura_AP1_P2.Services;
+
+public class CombosService
 {
-    public class CombosService(Contexto contexto)
+    private readonly IDbContextFactory<Contexto> _dbFactory;
+
+    public CombosService(IDbContextFactory<Contexto> dbFactory)
     {
-       
-        public async Task<List<Combos>> Listar(Expression<Func<Combos, bool>> criterio)
-        {
-            return await contexto.Combos.Include(c => c.CombosDetalles)
-                .ThenInclude(cd => cd.Articulo)  // Incluir los detalles de los artículos asociados
-                .Where(criterio)
-                .AsNoTracking()
-                .ToListAsync();
-        }
+        _dbFactory = dbFactory;
+    }
 
-        public async Task<Combos?> ObtenerPorId(int comboId)
-        {
-            return await contexto.Combos.Include(c => c.CombosDetalles)
-                .ThenInclude(cd => cd.Articulo)
-                .FirstOrDefaultAsync(c => c.ComboId == comboId);
-        }
+    public async Task<List<Combos>> Listar(Expression<Func<Combos, bool>> criterio)
+    {
+        await using var contexto = await _dbFactory.CreateDbContextAsync();
+        return await contexto.Combos
+            .Include(c => c.CombosDetalles)
+            .ThenInclude(cd => cd.Articulo)
+            .Where(criterio)
+            .AsNoTracking()
+            .ToListAsync();
+    }
 
-        public async Task<Combos> Crear(Combos combo)
+    public async Task<Combos?> ObtenerPorId(int comboId)
+    {
+        await using var contexto = await _dbFactory.CreateDbContextAsync();
+        return await contexto.Combos
+            .Include(c => c.CombosDetalles)
+            .ThenInclude(cd => cd.Articulo)
+            .FirstOrDefaultAsync(c => c.ComboId == comboId);
+    }
+
+    public async Task<Combos> Crear(Combos combo)
+    {
+        await using var contexto = await _dbFactory.CreateDbContextAsync();
+        contexto.Combos.Add(combo);
+        await contexto.SaveChangesAsync();
+        return combo;
+    }
+
+    public async Task<Combos> Modificar(Combos combo)
+    {
+        await using var contexto = await _dbFactory.CreateDbContextAsync();
+        contexto.Combos.Update(combo);
+        await contexto.SaveChangesAsync();
+        return combo;
+    }
+
+    public async Task<bool> Eliminar(int comboId)
+    {
+        await using var contexto = await _dbFactory.CreateDbContextAsync();
+        var combo = await contexto.Combos.FindAsync(comboId);
+
+        if (combo != null)
         {
-            contexto.Combos.Add(combo);
+            contexto.Combos.Remove(combo);
             await contexto.SaveChangesAsync();
-            return combo;
+            return true;
         }
 
-        public async Task<Combos> Modificar(Combos combo)
-        {
-            contexto.Combos.Update(combo);
-            await contexto.SaveChangesAsync();
-            return combo;
-        }
-
-        public async Task<bool> Eliminar(int comboId)
-        {
-            var combo = await contexto.Combos.FindAsync(comboId);
-            if (combo != null)
-            {
-                contexto.Combos.Remove(combo);
-                await contexto.SaveChangesAsync();
-                return true;
-            }
-            return false;
-        }
-
+        return false;
     }
 }
-
-
-
-
-      
-
-     
-
-
-
-   
-
-
-
-
